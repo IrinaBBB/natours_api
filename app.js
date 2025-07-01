a89231ff9f43f30853acc6e029b25f427708356d
IrinaB <balkarovairina@gmail.com>
2025-07-01 14:58:11 +0200

    add initial api setup for tours

--- app.js
+++ app.js
@@ -1,14 +1,76 @@
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'hello from server', app: 'Natours' })
+app.use(express.json())
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours,
+        },
+    })
 })
 
-app.post('/', (req, res) => {
-    res.json({ message: 'Hello from Post' })
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1
+    const tour = tours.find(tour => tour.id === id)
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({
+            id: newId,
+        },
+        req.body,
+    )
+
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        })
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated tour',
+        },
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    })
+})
+
 
 const port = 3000
 app.listen(port, () => {
