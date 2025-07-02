e5b4cf042d1cb0607c4668671062c31a8baa970a
IrinaB <balkarovairina@gmail.com>
2025-07-02 13:35:34 +0200

    add initial api setup for users and refactoring

--- app.js
+++ app.js
@@ -1,78 +1,28 @@
-const fs = require('fs')
 const express = require('express')
+const morgan = require('morgan')
+const tourRouter = require('./routes/tourRoutes.js')
+const userRouter = require('./routes/userRoutes.js')
 
 const app = express()
 
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 app.use(express.json())
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours,
-        },
-    })
+app.use(express.static(`${__dirname}/public`))
+app.use((req, res, next) => {
+    console.log('hello from middleware')
+    next()
 })
-
-app.get('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1
-    const tour = tours.find(tour => tour.id === id)
-
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        })
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour,
-        },
-    })
-})
-
-app.post('/api/v1/tours', (req, res) => {
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({
-            id: newId,
-        },
-        req.body,
-    )
-
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour,
-            },
-        })
-    })
-})
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated tour',
-        },
-    })
-})
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    })
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    next()
 })
 
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`App running on port ${port}`)
-})
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
+
+
+module.exports = app
