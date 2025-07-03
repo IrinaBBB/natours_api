bda5d302aa8805170000bca5ecf269140def912e
IrinaB <balkarovairina@gmail.com>
2025-07-03 23:07:43 +0200

    add API features for sorting, filtering etc

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const tourRouter = require('./routes/tourRoutes.js')
 const userRouter = require('./routes/userRoutes.js')
+const qs = require('qs')
 
 const app = express()
-
-
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+app.set('query parser', (str) => qs.parse(str))
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,29 @@
 const Tour = require('./../models/tourModel.js')
+const APIFeatures = require('./../utils/apiFeatures.js')
+
+exports.aliasTopTours = async (req, res, next) => {
+    req.url =
+        '/?sort=-ratingsAverage,price&fields=ratingsAverage,price,name,difficulty,summary&limit=5'
+    next()
+}
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find()
+        // EXECUTE QUERY
+        // Create a base query
+        const query = Tour.find()
+
+        // Apply API features
+        const features = new APIFeatures(query, req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate()
+
+        // Execute the final query
+        const tours = await features.query
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
@@ -89,3 +110,109 @@ exports.deleteTour = async (req, res) => {
         })
     }
 }
+
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } },
+            },
+            {
+                $group:
+                    {
+                        _id: { $toUpper: '$difficulty' },
+                        // _id: '$ratingsAverage',
+                        num: { $sum: 1 },
+                        numOfRatings: { $sum: '$ratingsQuantity' },
+                        avgRating: { $avg: '$ratingsAverage' },
+                        avgPrice: { $avg: '$price' },
+                        minPrice: { $min: '$price' },
+                        maxPrice: { $max: '$price' },
+                    },
+            },
+            {
+                $sort: { avgPrice: 1 },
+            },
+            {
+                $match: { _id: { $ne: 'EASY' } },
+            },
+        ])
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats,
+            },
+        })
+    } catch (e) {
+        res.status(400).json({
+            status: 'fail',
+            message: e,
+        })
+    }
+}
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = Number(req.params.year)
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates',
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    },
+                },
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTours: { $sum: 1 },
+                    tours: { $push: '$name' },
+                },
+            },
+            {
+                $addFields: { month: '$_id' },
+            },
+            {
+                $project: { _id: 0 },
+            },
+            {
+                $sort: { numTourStats: -1 },
+            },
+            {
+                $limit: 6
+            }
+        ])
+        res.status(200).json({
+            status: 'success',
+            results: plan.length,
+            data: {
+                plan,
+            },
+        })
+    } catch (e) {
+        res.status(400).json({
+            status: 'fail',
+            message: e,
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
