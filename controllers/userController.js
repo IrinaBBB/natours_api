8a82ef82d697ab841ebdc3def28729e436c634c2
IrinaB <balkarovairina@gmail.com>
2025-07-09 10:45:13 +0200

    add implementing user authentication
    Adds user signup and login functionality with JWT-based authentication.
    
    This introduces the `authController` to handle user registration and login.
    It also updates the `userModel` to include password hashing using bcrypt
    and integrates `jsonwebtoken` for secure token generation.
    
    The changes ensure that user passwords are securely stored and that only
    authenticated users can access protected resources.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yes defined',
+const User = require('./../models/userModel.js')
+const APIFeatures = require('./../utils/apiFeatures.js')
+const catchAsync = require('./../utils/catchAsync.js')
+const AppError = require('./../utils/appError.js')
+
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find()
+
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users,
+        },
     })
-}
+})
 
 exports.getUser = (req, res) => {
     res.status(500).json({
